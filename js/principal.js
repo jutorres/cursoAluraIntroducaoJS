d6caa562393bda565c3a089ce5cc79b77a6c5a81
jutorres <torresjujuliana@gmail.com>
2018-02-06 18:31:22 -0300

    Class 3 completed

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,28 @@
 var titulo = document.querySelector(".titulo");
-console.log(titulo);
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = document.querySelector(".info-peso");
+var tdAltura = document.querySelector(".info-altura");
+var tdImc = document.querySelector(".info-imc");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var pesoValido = true;
+var alturaValida = true;
+
+if(peso <= 0 || peso >= 500) {
+  tdImc.textContent = "Peso inválido!";
+  pesoValido = false;
+}
+
+if(altura <= 0 || altura >= 3.00) {
+  tdImc.textContent = "Altura inválida!";
+  alturaValida = false;
+}
+
+if(pesoValido && alturaValida) {
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc;
+}
