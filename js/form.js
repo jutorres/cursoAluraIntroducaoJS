6a78b49bb5a4dcbe8177e3bdb40460513781e437
jutorres <torresjujuliana@gmail.com>
2018-03-26 18:38:16 -0300

    Class 9 completed

--- js/form.js
+++ js/form.js
@@ -4,7 +4,6 @@ botaoAdicionar.addEventListener("click", function(event) {
   event.preventDefault();
   var form = document.querySelector("#form-adiciona");
   var paciente = obtemPacienteDoFormulario(form);
-  var pacienteTr = montaTr(paciente);
 
   var erros = validaPaciente(paciente);
 
@@ -13,10 +12,16 @@ botaoAdicionar.addEventListener("click", function(event) {
     return;
   }
 
-  var tabela = document.querySelector("#tabela-pacientes");
-  tabela.appendChild(pacienteTr);
+  adicionaPacienteNaTabela(paciente);
+
   form.reset();
 });
+
+function adicionaPacienteNaTabela(paciente) {
+  var pacienteTr = montaTr(paciente);
+  var tabela = document.querySelector("#tabela-pacientes");
+  tabela.appendChild(pacienteTr);
+}
 
 function obtemPacienteDoFormulario(form) {
   var paciente = {
